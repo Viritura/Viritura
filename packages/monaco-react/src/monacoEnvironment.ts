67a2c5768b4ca26dc005cd674c633ba0e8d35655
Peter Yang <me@peteryang.io>
2026-09-11 09:17:33 -0700

    fix(deps): update Monaco worker export paths

--- packages/monaco-react/src/monacoEnvironment.ts
+++ packages/monaco-react/src/monacoEnvironment.ts
@@ -1,5 +1,5 @@
-import EditorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-import JsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
+import EditorWorker from "monaco-editor/editor/editor.worker.js?worker";
+import JsonWorker from "monaco-editor/language/json/json.worker.js?worker";
 
 interface MonacoEnvironment {
   getWorker(moduleId: string, label: string): Worker;
